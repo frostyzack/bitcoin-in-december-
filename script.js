27c4b5fbc5460a1cac9ae4b056d0187214ec6912
frostyzack <zakariyyachawdhury@gmail.com>
2021-02-18 16:19:27 +0000

    done lol

---
+++ script.js
@@ -0,0 +1,12 @@
+async function getBTC(){
+    const endpoint = "https://api.coinbase.com/v2/prices/spot?currency=USD"
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const price = data.data.amount;
+    var priceOnDec1 = 19633.77;
+    var amountOfCoinIfYouPutIn1k = 1000 / priceOnDec1;
+    var amount = Math.round(((price * amountOfCoinIfYouPutIn1k) + Number.EPSILON) * 100) / 100;
+    document.getElementById("btc-price").textContent = price;
+    document.getElementById("btc-amount").textContent = amount;
+}
+getBTC();
