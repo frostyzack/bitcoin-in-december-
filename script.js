ea08f79fb3110900b2bbc57b01a0cfa533126acd
frostyzack <zakariyyachawdhury@gmail.com>
2021-03-01 00:23:17 +0000

    animated some text

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ async function getBTC(){
     var priceOnDec1 = 19633.77;
     var amountOfCoinIfYouPutIn1k = 1000 / priceOnDec1;
     var amount = Math.round(((price * amountOfCoinIfYouPutIn1k) + Number.EPSILON) * 100) / 100;
-    document.getElementById("btc-price").textContent = price;
+    //document.getElementById("btc-price").textContent = price;
     document.getElementById("btc-amount").textContent = amount;
+
+    var txt = price;
+    var i = 0;
+    var speed = 135;
+    function typeWriter() {
+        if (i < txt.length) {
+          document.getElementById("btc-price").innerHTML += txt.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
+        }
+      }
+      typeWriter();
 }
 getBTC();
+
+
+
